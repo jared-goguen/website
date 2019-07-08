bf4a7e11223106c09c7541cd9984f86046b01344
Jared Goguen <jgoguen@wayfair.com>
2019-07-07 20:07:09 -0400

    Fixing mongo config parameter names.

--- server/mongcon.js
+++ server/mongcon.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-const url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 mongoose.connect(url);
 
 module.exports = exports = mongoose;
